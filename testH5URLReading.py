aa486295da16c4a106be4a4b0d72e21f5adff89c
Romain Bouquet <romain.bouquet@cern.ch>
2026-01-31 03:59:52 +0100

    Adding changes

--- testH5URLReading.py
+++ testH5URLReading.py
@@ -31,6 +31,6 @@ fileName= "/home/rbouquet/eos/umamiTest/inputs/user.treisch.601589.e8547_s3797_r
 fileName= "davs://grid03.ge.infn.it:8443/webdav/atlaslocalgroupdisk/rucio/user/treisch/6b/2e/user.treisch.48141599._000002.output.h5"
 
 
-# with getH5File(fileName) as f: 
-#   print(list(hf.keys()))
+with getH5File(fileName) as f: 
+  print(list(hf.keys()))
   
